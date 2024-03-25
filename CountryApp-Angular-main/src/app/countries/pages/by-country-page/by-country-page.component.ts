3d20cef2209a5fbd890ab5b5ab22d52d2ae04897
SaidIa0703 <saidi_a@etna-alternance.net>
2024-03-25 10:58:38 +0100

    finishAa

---
+++ CountryApp-Angular-main/src/app/countries/pages/by-country-page/by-country-page.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { Country } from '../../interfaces/country';
+import { CountryService } from '../../services/countries.service';
+
+@Component({
+  selector: 'app-by-country-page',
+  templateUrl: './by-country-page.component.html',
+  styles: [
+  ]
+})
+export class ByCountryPageComponent implements OnInit {
+
+  public countries: Country[] = [];
+  public isLoading: boolean = false;
+  public initialValue: string = "";
+
+  constructor( private countryService: CountryService ) {}
+
+  ngOnInit(): void {
+    this.countries = this.countryService.cacheStore.byCountries.countries;
+    this.initialValue = this.countryService.cacheStore.byCountries.term;
+  }
+
+  searchByCountry( term: string ):void {
+    this.isLoading = true;
+
+    this.countryService.searchCountry( term )
+      .subscribe( countries => {
+        this.countries = countries;
+        this.isLoading = false;
+      });
+  }
+
+}
